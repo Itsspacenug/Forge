ba053a59299a5fe5277b0dfcc548a9415a4c4782
Itsspacenug <aaln22006@gmail.com>
2026-04-04 22:27:06 -0600

    added connection from OptimizerPage to ResultsPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,18 @@
+import { useState } from 'react'
 import OptimizerPage from './pages/OptimizerPage'
+import ResultsPage from './pages/ResultsPage'
 
 export default function App() {
-  return <OptimizerPage />
+  const [selectedSchedule, setSelectedSchedule] = useState(null)
+
+  if(selectedSchedule){
+      return ( 
+      <ResultsPage
+        schedule={selectedSchedule}
+        onBack={() => setSelectedSchedule(null)} 
+        />
+      )
+  }
+
+  return <OptimizerPage onSelectSchedule={setSelectedSchedule} />
 }
